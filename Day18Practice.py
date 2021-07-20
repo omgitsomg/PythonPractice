70d51af77ada2623af43276218dc094af83de842
Kevin Ly <lykt2@mymail.vcu.edu>
2021-07-19 23:52:35 -0400

    This is part 2 of day 18 of python practice.

--- Day18Practice.py
+++ Day18Practice.py
@@ -51,3 +51,51 @@ foo1(arg = "cool")
 #foo1("cool")
 foo2("first arg", "second arg", 1, 2, 3, 4, 5)
 foo3("Good Morning", "Positional", "Argument", "Two", one = "1", two = "2", three = "3")
+
+def foobar(num, arr=[]):
+    arr.append(num)
+    return arr
+
+print(foobar(1))
+print(foobar(2))
+print(foobar(3))
+print(foobar(4))
+
+# The function foobar creates a List, but that list is shared between all calls.
+# To create a function that makes a separate List for every call, use the function
+# below this comment.
+
+def correctFoobar(num, arr = None):
+    if arr is None:
+        arr = []
+
+    arr.append(num)
+    return arr
+
+print(correctFoobar(1))
+print(correctFoobar(2))
+print(correctFoobar(3))
+print(correctFoobar(4))
+
+# Python currently has no switch/case statement.
+# One way to implement a switch/case statement is to use a sequence of if -> elif -> ... -> elif -> else.
+# Another way is to createa  function to do the switch/case statement.
+
+def switchcase1(str):
+    print("1st switch case function:", str)
+    pass
+def switchcase2(str):
+    print("2nd switch case function:", str)
+    pass
+def switchcase3(str):
+    print("3rd switch case function:", str)
+    pass
+
+str = "VCU"
+switchcaseDict = {"a" : switchcase1,
+                  "b" : switchcase2,
+                  "c" : switchcase3}
+
+value = "b"
+function = switchcaseDict[value]
+function(str)
